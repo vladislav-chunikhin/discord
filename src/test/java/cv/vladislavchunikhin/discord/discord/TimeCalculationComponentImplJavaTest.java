4a05f6201195815ccefeea8fbf762f6307431865
Vladislav Chunikhin <vladislav.chunikhin.95@gmail.com>
2021-02-21 23:17:41 +0400

    master, editing java unit tests

--- src/test/java/cv/vladislavchunikhin/discord/discord/TimeCalculationComponentImplJavaTest.java
+++ src/test/java/cv/vladislavchunikhin/discord/discord/TimeCalculationComponentImplJavaTest.java
@@ -2,12 +2,14 @@ package cv.vladislavchunikhin.discord.discord;
 
 import cv.vladislavchunikhin.discord.discord.dto.SecPeriodDto;
 import cv.vladislavchunikhin.discord.discord.dto.TimeCalculationDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-import java.util.stream.IntStream;
+import java.time.Month;
 import java.util.stream.Stream;
 
 public class TimeCalculationComponentImplJavaTest {
@@ -16,12 +18,17 @@ public class TimeCalculationComponentImplJavaTest {
 
     @ParameterizedTest
     @MethodSource("calculateSecPeriodFromNowToFixedTimeProvider")
-    void calculateSecPeriodFromNowToFixedTime(final TimeCalculationDto inputDto) {
+    void calculateSecPeriodFromNowToFixedTime(final TimeCalculationDto inputDto, long expectedDelay, long expectedPeriod) {
         SecPeriodDto actualResult = testable.calculateSecPeriodFromNowToFixedTime(inputDto);
+        Assertions.assertEquals(actualResult.getDelay(), expectedDelay);
+        Assertions.assertEquals(actualResult.getPeriod(), expectedPeriod);
     }
 
-    static Stream<TimeCalculationDto> calculateSecPeriodFromNowToFixedTimeProvider() {
-        LocalDateTime now = LocalDateTime.now();
-        return IntStream.range(0, 24).mapToObj(index -> new TimeCalculationDto(now.getDayOfWeek(), index, now));
+    static Stream<Object[]> calculateSecPeriodFromNowToFixedTimeProvider() {
+        final LocalDateTime currentTime = LocalDateTime.of(2021, Month.JANUARY, 4, 2, 23);
+        return Stream.of(
+               new Object[]{new TimeCalculationDto(DayOfWeek.MONDAY, 0, currentTime), 0, 604800},
+               new Object[]{new TimeCalculationDto(DayOfWeek.MONDAY, 1, currentTime), 0, 604800}
+        );
     }
 }
